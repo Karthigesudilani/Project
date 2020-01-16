9043ab365ab2b00716ee9c44d4ba8a1a52758779
Karthigesu Dilani <karthigesudilani21@gmail.com>
2020-01-16 15:22:59 +0530

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,482 @@
+
+
+<HTML lang="en">
+<HEAD>
+	<TITLE>
+	Tourists Transportation and Guiding Management System for a Travel Agency In Badulla	
+	</TITLE>
+	  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+  	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	
+
+<!-- CUSTOM STYLE CSS -->
+    <link href="ass\style.css" rel="stylesheet" />
+
+
+			<style>
+
+			* {
+    			box-sizing: border-box;
+				}
+	
+@media screen and(min-width:600px ){
+	#image{
+			flex:50%;
+			max-width: 50%;
+			}
+	}
+
+@media screen and(max-width:800px ){
+	#image{
+			flex:100%;
+			max-width: 100%;
+			}
+	}
+
+.div1 {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 4px;
+}
+
+/* Create four equal columns that sits next to each other */
+.image {
+    flex: 25%;
+    max-width: 25%;
+    padding: 0 4px;
+	background-color :black;
+	flex-wrap: nowrap;
+}
+
+.img:hover{
+transform: scale(1.15);
+}
+.img{
+    margin-top: 10px;
+ }
+
+.full {
+ 
+    width: 100%; 
+ 
+}
+ 
+ 
+ 
+.footer {
+ 
+                height: auto;
+ 
+                padding-bottom: 30px;
+	
+                position: relative;
+ 
+                width: 100%;
+ 
+                 color:white;
+ 
+                border-bottom: 1px solid #CCCCCC;
+ 
+                border-top: 1px solid #DDDDDD;
+ 
+                background: #ffffff;
+ 
+                background: -webkit-linear-gradient(to right, #000000, #ffffff); 
+ 
+                background: linear-gradient(to right, #000000, #ffffff);   
+ 
+}
+ .full-width{
+	 width:100%;
+	 background:#ccc;
+ }
+ 
+ 
+ 
+ 
+
+.footer ul {
+ 
+                font-size: 13px;
+ 
+                list-style-type: none;
+ 
+                margin-left: 0;
+ 
+                padding-left: 0;
+ 
+                margin-top: 15px;
+ 
+                color: #000000;
+ 
+}
+
+
+ 
+
+</style>
+</HEAD>
+
+<BODY>
+<div style="background-color: #FADBD8;">
+
+<div>
+  <center><h2 style="color:navy;font-family:serif;"><b>Tourists Transportation and Guiding Management System for a Travel Agency In Badulla.
+</h2></center></div>
+
+<!--Nav Bar-->	
+<div>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link active" href="Home.php">Home</a>
+    </li>
+ 
+     <li class="nav-item">
+      <a class="nav-link" href=""></a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="Transportation.php">Transport</a>
+    </li>
+        
+
+     <li class="nav-item">
+      <a class="nav-link" href=""></a>
+    </li>
+    
+    <li class="nav-item">
+      <a class="nav-link" href="guide.php">Guide</a>
+    </li>
+
+     <li class="nav-item">
+      <a class="nav-link" href=""></a>
+    </li>
+ 
+     <li class="nav-item">
+      <a class="nav-link" href="driver.php">Driver</a>
+    </li>
+  
+     <li class="nav-item">
+      <a class="nav-link" href=""></a>
+    </li>
+    
+     <li class="nav-item">
+      <a class="nav-link" href="package.php">Package</a>
+    </li>
+   
+     <li class="nav-item">
+      <a class="nav-link" href=""></a>
+    </li>
+  
+    
+    <li class="nav-item">
+      <a class="nav-link" href="places.php">Tourist Attraction Places</a>
+    </li>
+  
+    <li class="nav-item">
+      <a class="nav-link" href=""></a>
+    </li>
+    
+    
+      <li class="nav-item">
+      <a class="nav-link" href="about.php">About Us</a>
+    </li>
+
+     <li class="nav-item">
+      <a class="nav-link" href=""></a>
+    </li>
+   
+ <li class="nav-item" >
+      <a class="nav-link" href="cancelbooking.php">Cancel Booking</a>
+    </li>
+
+     <li class="nav-item">
+      <a class="nav-link" href=""></a>
+    </li>
+
+   <li class="nav-item" >
+      <a class="nav-link" href=""><img src="img\add.png" /></a>
+    </li>
+ <li class="nav-item">
+      <a class="nav-link" href=""></a>
+    </li>
+     <li class="nav-item">
+      <a class="nav-link" href=""></a>
+    </li>
+    <li class="nav-item" >
+  <a class="nav-link" href="login.php"><img src="img\log.png" /></a>
+    </li>
+    
+    </li>
+  </ul>
+</nav>
+</div>
+<div style = "background-image:url('new2.jpg');  background-repeat: no-repeat;
+  background-attachment: scroll;  
+  background-size: 100%50%;">
+    <!--HOME SECTION-->
+     
+ 
+        <div class="row text-center">
+            <div  class="col-md-13 >            
+<div class="item">
+ <br />
+  <br />
+   <br />
+    <br />
+     <br />
+      <br /> <br />
+       <br />
+        <br />
+         <br />
+        <br />
+		 
+		 <h1 style="color:cornsilk; font-family:Arial">
+		<center>
+			WELCOME TO BADULLA
+		</center>	</h1> 
+         <br />
+          <br />
+           <br /> <br />
+            <br />
+             <br />
+              <br />
+               <br />
+				 <br />
+					<br />
+				  <br />
+				   <br />
+			   <br />
+				 <br />
+					<br />
+				  <br />
+				   <br />
+               
+  
+
+                  
+    <!--SERVICES SECTION-->
+	
+          
+							<body style="background-color: #FADBD8;">
+                            <h3 class="full-width" style="color:black"> <div class="wrap">Our Service</div></h3>
+                           
+                    </center>
+                	<table>
+                		<tr>
+                			<td style="padding:30px">
+                    <div class="col-md-11 col-sm- alert-success">
+                            <h4 style="color:black">Transport</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                            
+                    </div>
+                </td>
+                <td style="padding:30px">
+                    <div class="col-md-11 col-sm-11 alert-success">
+                            <h4 style="color:black"> Guide </h4>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                            
+                    </div>
+                   </td>
+               </tr>
+               <tr>
+                			<td style="padding:30px">
+                    <div class="col-md-11 col-sm-11 alert-success">
+                            <h4 style="color:black">Driver</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                            
+                    </div>
+                </td>
+                <td style="padding:30px">
+                    <div class="col-md-11 col-sm-11 alert-success">
+                            <h4 style="color:black">Provide Places Information </h4>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                            
+                    </div>
+                   </td>
+               </tr>
+</table></div></div></div>
+<br />
+<br />                
+          	<div class="text-center">
+                	<center><div class="col-md-4 col-sm-4 alert-info">
+                          <br /><h3>Transport</h3>
+                           
+                    </div></center>
+           <div class="div1">
+		<div class="image">
+<div class="img"><img src="img\car.jpg" width="100%" ></div>
+</div><div class="image">
+<div class="img"><img src="img\jeep.jpg" width="100%" ></div>
+</div><div class="image">
+<div class="img"><img src="img\bus.jpeg" width="100%" ></div>
+</div><div class="image">
+<div class="img"><img src="img\van.jpg" width="100%" height="90%"></div>
+</div></div><br><br>
+
+  <a class="nav-link" href="Transportation.php"><button type="button" class="btn btn-success" style="align-items: right;"><center>Get Start Booking For Transport</button></a>
+
+                    </div></center>
+    </div>
+
+                  
+<br />
+<br />
+          	 <div class="text-center">
+                	<center><div class="col-md-4 col-sm-4 alert-info">
+                            <br /><h3> Driver</h3>
+                           
+                    </div></center>
+           <div class="div1">
+		<div class="image">
+<div class="img"><img src="img\dr1.jpg" width="100%" height="90%"></div>
+</div><div class="image">
+<div class="img"><img src="img\dr2.jpg" width="100%" height="90%"></div>
+</div><div class="image">
+<div class="img"><img src="img\dr3.jpg" width="100%" height="90%"></div>
+</div><div class="image">
+<div class="img"><img src="img\driver.jpg" width="100%"height="90%"></div>
+</div></div>
+
+  <a class="nav-link" href="driver.php"><button type="button" class="btn btn-success" ><center>Get Start Booking For Driver</button></a>
+ </center></div>   
+
+</div> 
+      <br />
+      <br />   
+         	<div class="text-center">
+                	<center><div class="col-md-4 col-sm-4 alert-info">
+                          <br /><h3>Guide</h3>
+                           
+                    </div></center>
+           <div class="div1">
+		<div class="image">
+<div class="img"><img src="img\guu.jpg" width="100%" height="95%"></div>
+</div><div class="image">
+<div class="img"><img src="img\gu3.jpg" width="100%" height="95%"></div>
+</div><div class="image">
+<div class="img"><img src="img\gu6.jpg" width="100%" height="95%"></div>
+</div><div class="image">
+<div class="img"><img src="img\gu5.jpg" width="100%"height="95%"></div>
+</div></div>
+
+  <a class="nav-link" href="Transportation.php"><button type="button" class="btn btn-success"><center>Get Start Booking For Guide</button></a>
+
+                    </div></center>
+    </div>
+      
+<br><br>
+
+<footer>
+
+ 
+    <div class="footer" id="footer">
+ 
+        <div class="container">
+ 
+            <div class="row">
+ 
+                <div class="col-lg-3  col-md-3 col-sm-5 col-xs-7">
+ 
+                    <h3><br> Services </h3>
+ 
+                    <ul>
+ 
+                        <li> <a href="Transportation.php"> Transportation </a> </li>
+ 
+                        <li> <a href="guide.php"> Guide </a> </li>
+ 
+                        <li> <a href="driver.php"> Driver</a> </li>
+ 
+                        
+                    </ul>
+ 
+                </div>
+ 
+                <div class="col-lg-3  col-md-3 col-sm-5 col-xs-7">
+ 
+                    <h3><br> About </h3>
+ 
+                    <ul>
+ 
+                        <li> <a href="#"> Our Company </a> </li>
+ 
+                        <li> <a href="#"> About Us </a> </li>
+ 
+                        <li> <a href="#"> Terms of Services </a> </li>
+ 
+                        <li> <a href="#"> Our Team </a> </li>
+ 
+                    </ul>
+ 
+                </div>
+ 
+                <div class="col-lg-3  col-md-3 col-sm-5 col-xs-7">
+ 
+                    <h3> <br>Contact </h3>
+ 
+                    <ul>
+ 
+                       <li> <a href="www.facebook.com"> <i class=" fa fa-facebook">   </i> </a> </li>
+ 
+                        <li> <a href="#"> <i class="fa fa-twitter">   </i> </a> </li>
+ 
+                        <li> <a href="#"> <i class="fa fa-google-plus">   </i> </a> </li>
+ 
+                        <li> <a href="#"> <i class="fa fa-pinterest">   </i> </a> </li>
+ 
+                        <li> <a href="#"> <i class="fa fa-youtube">   </i> </a> </li>
+ 
+ 
+                    </ul>
+ 
+                </div>
+ 
+                <div class="col-lg-3  col-md-3 col-sm-6 col-xs-7 ">
+ 
+                    <h3><br> Rating  </h3>
+ 
+ 
+      <div class="rating">
+<ul><li><b><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></b></li></ul>
+</div>     
+          </div>
+ 
+            </div>
+ 
+        </div>
+ 
+        
+    </div>
+ 
+       </div>
+</footer>
+</div>
+
+</BODY>
+</HTML>
